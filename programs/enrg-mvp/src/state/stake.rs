6068da963c1d3fab234cb1f8b261b829cfa541d2
ENRG <enrg.project@gmail.com>
2026-07-13 07:14:55 +0300

    feat(contract): extend staking state

--- programs/enrg-mvp/src/state/stake.rs
+++ programs/enrg-mvp/src/state/stake.rs
@@ -7,10 +7,22 @@ pub struct StakeInfo {
 
     /// Amount currently staked.
     pub staked_amount: u64,
+
+    /// Pending rewards.
+    pub pending_rewards: u64,
+
+    /// Stake creation timestamp.
+    pub started_at: i64,
+
+    /// Last rewards update.
+    pub last_update: i64,
 }
 
 impl StakeInfo {
     pub const LEN: usize =
         32 + // owner
-        8;   // staked_amount
+        8  + // staked_amount
+        8  + // pending_rewards
+        8  + // started_at
+        8;   // last_update
 }
